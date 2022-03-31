8e30f8b531c69894a1b3b1fd2cb958f6ea1e7af8
Jeremy <tommyhilfiger12@hotmail.com>
2022-03-31 14:21:33 -0500

    booklist

--- src/index.js
+++ src/index.js
@@ -3,44 +3,42 @@ import ReactDOMClient from 'react-dom/client';
 import './index.css';
 
 // setup variables
-const firstBook = {
-	img: 'https://m.media-amazon.com/images/I/81WWiiLgEyL._SX140_.jpg',
-	title: 'Where the Crawdads Sing',
-	author: 'by Delia Owens',
-	href: 'https://www.amazon.com/Delia-Owens/e/B000AP8MAK/ref=dp_byline_cont_pop_ebooks_1',
-};
-const secondBook = {
-	img: 'https://m.media-amazon.com/images/I/9151RYMhsQL.jpghttps://m.media-amazon.com/images/I/81WWiiLgEyL._SX140_.jpg',
-	title: 'Run, Rose, Run',
-	author: 'by James Patterson & Dolly Parton',
-	href: 'https://www.amazon.com/s?k=James+Patterson&i=audible&ref=dp_byline_sr_audible_1',
-};
-
+const books = [
+	{
+		id: 1,
+		img: 'https://m.media-amazon.com/images/I/81WWiiLgEyL._SX140_.jpg',
+		title: 'Where the Crawdads Sing',
+		author: 'by Delia Owens',
+		href: 'https://www.amazon.com/Delia-Owens/e/B000AP8MAK/ref=dp_byline_cont_pop_ebooks_1',
+	},
+	{
+		id: 2,
+		img: 'https://m.media-amazon.com/images/I/9151RYMhsQL.jpghttps://m.media-amazon.com/images/I/81WWiiLgEyL._SX140_.jpg',
+		title: 'Run, Rose, Run',
+		author: 'by James Patterson & Dolly Parton',
+		href: 'https://www.amazon.com/s?k=James+Patterson&i=audible&ref=dp_byline_sr_audible_1',
+	},
+];
 // functions
 function BookList() {
 	return (
 		<section className="booklist">
-			<Book
-				img={firstBook.img}
-				title={firstBook.title}
-				author={firstBook.author}
-				href={firstBook.href}
-			/>
-			<Book
-				img={secondBook.img}
-				title={secondBook.title}
-				author={secondBook.author}
-				href={secondBook.href}
-			/>
+			{books.map((book) => (
+				<Book key={book.id} {...book} />
+			))}
 		</section>
 	);
 }
-const Book = ({img,title,author,href}) => {
+const Book = ({ img, title, author, href }) => {
+	const clickHandler = () => {
+		alert('moo');
+	};
 	return (
 		<article className="book">
 			<img src={img} alt="" />
 			<h1>{title}</h1>
 			<a href={href}>{author}</a>
+			<button onClick={clickHandler} type="button">example</button>
 		</article>
 	);
 };
