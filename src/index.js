83052e3c9307beb068d0ae9472114c8b7fba4af3
Jeremy <tommyhilfiger12@hotmail.com>
2022-03-30 20:38:59 -0500

    props

--- src/index.js
+++ src/index.js
@@ -1,43 +1,48 @@
 import ReactDOMClient from 'react-dom/client';
+// css
+import './index.css';
 
-const root = ReactDOMClient.createRoot(document.getElementById('root'));
+// setup variables
+const firstBook = {
+	img: 'https://m.media-amazon.com/images/I/81WWiiLgEyL._SX140_.jpg',
+	title: 'Where the Crawdads Sing',
+	author: 'by Delia Owens',
+	href: 'https://www.amazon.com/Delia-Owens/e/B000AP8MAK/ref=dp_byline_cont_pop_ebooks_1',
+};
+const secondBook = {
+	img: 'https://m.media-amazon.com/images/I/9151RYMhsQL.jpghttps://m.media-amazon.com/images/I/81WWiiLgEyL._SX140_.jpg',
+	title: 'Run, Rose, Run',
+	author: 'by James Patterson & Dolly Parton',
+	href: 'https://www.amazon.com/s?k=James+Patterson&i=audible&ref=dp_byline_sr_audible_1',
+};
 
+// functions
 function BookList() {
 	return (
-		<section>
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
+		<section className="booklist">
+			<Book
+				img={firstBook.img}
+				title={firstBook.title}
+				author={firstBook.author}
+				href={firstBook.href}
+			/>
+			<Book
+				img={secondBook.img}
+				title={secondBook.title}
+				author={secondBook.author}
+				href={secondBook.href}
+			/>
 		</section>
 	);
 }
-
-const Book = () => {
+const Book = ({img,title,author,href}) => {
 	return (
-		<article>
-			<Image />
-			<Title />
-			<Author />
+		<article className="book">
+			<img src={img} alt="" />
+			<h1>{title}</h1>
+			<a href={href}>{author}</a>
 		</article>
 	);
 };
 
-const Image = () => (
-	<img
-		src="https://m.media-amazon.com/images/I/81WWiiLgEyL._SX140_.jpg"
-		alt=""
-	/>
-);
-
-const Title = () => <h1>Where the Crawdads Sing Kindle Edition</h1>;
-
-const Author = () => (
-	<a href="https://www.amazon.com/Delia-Owens/e/B000AP8MAK/ref=dp_byline_cont_pop_ebooks_1">
-		by Delia Owens
-	</a>
-);
-
-root.render(<BookList />);
+ReactDOMClient.createRoot(document.getElementById('root')).render(<BookList />);
